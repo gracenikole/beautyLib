a3d68e00e934580dcfe4bcb0f533c6c39cb0287f
gracenikole <grace.alvarado@utec.edu.pe>
2020-04-26 19:06:31 -0500

    :squirrel: calculator

--- calc.py
+++ calc.py
@@ -17,19 +17,18 @@ print("1.Sumar")
 print("2.Restar")
 print("3.Multiplicar")
 
-# Take input from the user 
-choice = input("Escoge una opción (1/2/3): ")
+option = input("Escoge una opción (1,2,3): ")
 
 num1 = float(input("Escribe el primer número: "))
 num2 = float(input("Escribe el segundo número: "))
 
-if choice == '1':
+if option == '1':
   print(num1,"+",num2,"=", add(num1,num2))
 
-elif choice == '2':
+elif option == '2':
   print(num1,"-",num2,"=", subtract(num1,num2))
 
-elif choice == '3':
+elif option == '3':
   print(num1,"*",num2,"=", multiply(num1,num2))
   
 else:
